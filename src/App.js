8cc1fa531ab4b1f53f759af71ef29efd417a6805
Suthipong C.vidhayakul <lufael.sc@gmail.com>
2023-08-09 14:56:37 +0700

    add bulma.io css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
+import "bulma/css/bulma.css";
+
+import ImgAlexa from "./images/alexa.png";
+import ImgCortana from "./images/cortana.png";
+import ImgSiri from "./images/siri.png";
 import ProfileCard from "./ProfileCard";
 
+// console.log(ImgAlexa);
+// console.log(ImgCortana);
+// console.log(ImgSiri);
+
 function App() {
-    return (
-        <div>
-            <div><h1>Personal Digital Assistants</h1></div>
-            
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cortana" handle="@cortana32"/>
-            <ProfileCard title="Siri" handle="@siri01"/>
+  return (
+    <div>
+      <div>
+        <h1>Personal Digital Assistants</h1>
+      </div>
+
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard title="Alexa" handle="@alexa99" image={ImgAlexa} />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={ImgCortana}
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard title="Siri" handle="@siri01" image={ImgSiri} />
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,16 +1,25 @@
 // function ProfileCard(props) {
-function ProfileCard({ title, handle }) {
-    // const title = props.title;
-    // const handle = props.handle;
+function ProfileCard({ title, handle, image }) {
+  // const title = props.title;
+  // const handle = props.handle;
 
-    // const { title, handle } = props;
-    console.log(title, handle);
-    return (
-        <div>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+  // const { title, handle } = props;
+  console.log(title, handle);
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt={title} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
